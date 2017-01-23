2528a3675b15c2752e5566a95d68615a9cb8b0dc
Noelphilip1990 <noyo1000gt@gmail.com>
2017-01-23 13:30:21 +0530

    Create app.js
    js file

---
+++ app.js
@@ -0,0 +1,15 @@
+var a=1;
+var b=8;
+var c=5;
+console.log("Given three numbers are: "+a+","+b+","+c);
+if(a>b && a>c)
+{
+	console.log(a+" is greater than "+b+" & "+c);
+}else if(b>a && b>c)
+{
+	console.log(b+" is greater than "+a+" & "+c);
+}
+else
+{
+	console.log(c+" is greater than "+a+" & " +b);
+}
